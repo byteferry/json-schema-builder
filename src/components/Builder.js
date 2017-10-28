0d5814e02a645d040e020c4e20c2cd2787d611f3
Jamie Lynch <James.Lynch@sky.uk>
2017-10-28 11:26:42 +0100

    Moved the build button outside of the form

--- src/components/Builder.js
+++ src/components/Builder.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Header, Button } from 'semantic-ui-react'
 import SchemaForm from './SchemaForm'
 
 class Builder extends Component {
+  constructor () {
+    super()
+
+    // bind functions
+    this.generateSchemaObject = this.generateSchemaObject.bind(this)
+  }
+
+  generateSchemaObject () {
+    this.refs._schema.generateSchemaObject()
+  }
+
   render () {
     return (
       <Segment attached className='Builder'>
         <Header as='h3'>Builder</Header>
-        <SchemaForm setSchema={this.props.setSchema} />
+        <SchemaForm ref='_schema' setSchema={this.props.setSchema} />
+        <Button primary onClick={this.generateSchemaObject}>Build</Button>
       </Segment>
     )
   }
